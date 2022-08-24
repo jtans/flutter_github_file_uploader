0758fdf611b6fe42f86c4cec760704534d002d29
tans <jtans@qq.com>
2022-08-24 21:08:17 +0800

    github file uploader

--- lib/flutter_github_file_uploader.dart
+++ lib/flutter_github_file_uploader.dart
@@ -1,11 +1,38 @@
 library flutter_github_file_uploader;
+
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class FlutterGithubFileUploader {
-  upload({
+  static Future<http.Response> upload({
     required String content,
     required String token,
-  }) {
-    http.put(Uri.https('authority', 'unencodedPath'));
+    required String username,
+    required String tag,
+    String branch = 'main',
+    String Function()? pathBuilder,
+  }) async {
+    final dateStr = DateTime.now().toIso8601String();
+    var url = '';
+    if (pathBuilder != null) {
+      url += pathBuilder();
+    } else {
+      url += '/repos/$username/files/contents/logs/$tag/$dateStr.txt';
+    }
+
+    var body = {
+      "message": dateStr,
+      "committer": {"name": "amp", "email": "user@163.com"},
+      "content": base64.encode(utf8.encode(content)),
+      "branch": branch,
+    };
+    return http.put(Uri.https('api.github.com', url),
+        headers: {
+          'Authorization': 'token $token',
+          // 'Content-Type': 'application/json',
+          'accept': 'application/vnd.github.v3+json'
+        },
+        body: jsonEncode(body));
   }
 }
